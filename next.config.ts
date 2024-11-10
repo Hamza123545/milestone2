057a5dfce18576d9833ff1dc64ec8b2b8eb6e19a
Muhammad Hamza <150414868+Hamza123545@users.noreply.github.com>
2024-11-10 19:23:24 +0500

    Added

---
+++ next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+// next.config.js
+
+module.exports = {
+  images: {
+    domains: ['via.placeholder.com', 'your-image-host.com'], // Add any other domains you might use here
+  },
+}
+
+
+export default nextConfig;
